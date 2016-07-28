36fa578cb4e567f2f071006320f8b469fd6ec7a3
zummyJP <zummyJP@users.noreply.github.com>
2016-07-28 20:18:02 +0900

    right index.js

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
-var express = require('express');
-var app = express();
+var NCMB = require('ncmb');
 
-app.set('port', (process.env.PORT || 5000));
+var application_key = process.env.APPLICATION_KEY;
+var client_key      = process.env.CLIENT_KEY;
+var ncmb = new NCMB(application_key, client_key);
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-
+var Heroku = ncmb.DataStore('Heroku');
+var heroku = new Heroku;
+heroku.set('text', 'Hello, from heroku.');
+heroku.save()
+  .then(function(o) {
+    console.log(o)
+  });
